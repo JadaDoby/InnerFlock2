6012873e1d36c8a0c00fbcb0643ef929cce4c88b
Jasmine Masopeh <jdmasopeh@outlook.com>
2024-03-19 10:23:10 -0400

    cleaned up code a lot and added comments to functions I created ps. we do not need forms.py

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,18 +1,18 @@
-from django import forms
+""" from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User  # Import User model
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import UserProfile """
 
-class SignUpForm(UserCreationForm):
+""" class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2'] """
 
-class UserForm(forms.Form):
-    name = forms.CharField(max_length=255, label='User Name')
+""" class UserForm(forms.Form):
+    name = forms.CharField(max_length=255, label='User Name') """
 
-class UserProfileForm(forms.ModelForm):
+""" class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['profile_picture', 'school_name']
+        fields = ['profile_picture', 'school_name'] """
